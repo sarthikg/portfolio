702c9638de670bf8afa1ac804774abaf52f88c87
Sarthik Gupta <sarthikg@gmail.com>
2021-03-29 21:17:30 +0530

    Completed Contact Page

---
+++ src/components/Contact.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className="Component-Contact">
+                {this.props.contacts.map((contact) => {
+					return (
+                        <div className="Component-Contact-Object">
+                            <a href={contact.url ? contact.url : undefined} target="_blank" rel="noreferrer">
+							    <img src={contact.icon} alt={contact.alt} height="20px"/>
+						    </a>
+                            <div className="Component-Contact-Object-Separator"/>
+                                {contact.content}   
+                        </div>
+					);
+				})}
+            </div>
+        )
+    }
+}
+
+export default Contact
--- src/layouts/Section5.js
+++ src/layouts/Section5.js
@@ -4,11 +4,30 @@ import Section from '../components/Section';
 import Heading from '../components/Heading';
 import SubHeading from '../components/SubHeading';
 import Social from '../components/Social';
+import Contact from '../components/Contact'
 
 import linkedin from '../linkedin.svg';
 import github from '../github.svg';
 import twitter from '../twitter.svg';
 import medium from '../medium.svg';
+
+import email from '../email.svg'
+import phone from '../phone.svg'
+
+let contacts = [
+	{
+		alt: 'email',
+		icon: email,
+		url: 'href="mailto:sarthikg@gmail.com"',
+		content: 'sarthikg@gmail.com'
+	},
+	{
+		alt: 'phone',
+		icon: phone,
+		url: null,
+		content: '+91-8872425152'
+	},
+]
 
 let socials = [
 	{
@@ -53,8 +72,8 @@ class Section5 extends Component {
 	render() {
 		return (
 			<Section>
-				<Heading title="Contact" />
 				<div className="App-Section5-Content">
+                    <Heading title="Contact" />
 					<div className="App-Section5-Content-Description">
 						I'm interested in full-time opportunities in the field of Full-Stack Development, Business
 						Analysis and Product Management. Feel free to hit me up by filling the form below. You can also
@@ -71,15 +90,17 @@ class Section5 extends Component {
 							</button>
 						</form>
 						<div className="App-Section5-Content-Container-Social">
-							<SubHeading title="Find me at" />
-							<Social socials={socials} />
-                            <div className="App-Section5-Content-Container-Social-Remarks">
-                                Design highly inspired from <a href="https://jacekjeznach.com/">Jack's Website</a>. There are no analytics setup with this website, hence no data was collected for any sort of usage.
-                                <br/>
-                                <br/>
-                                <br/>
-                                Drop suggestions and bugs at sarthikg@gmail.com
-                            </div>
+                        <SubHeading title="Find me at" />
+							<div className="App-Section5-Content-Container-Social-Details">
+								<Contact contacts={contacts}/>
+								<Social socials={socials} />
+								<div className="App-Section5-Content-Container-Social-Remarks">
+									Design is highly inspired from{' '}
+									<a href="https://jacekjeznach.com/">Jack's Website</a>.
+									<br/>
+									Copyright © 2021 All Rights Reserved.
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
