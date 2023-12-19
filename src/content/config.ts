36f2e0c3cd8817a55aa2fbd6d4c8fd632a936524
Sarthik Gupta <sarthikg@icloud.com>
2023-12-19 15:11:33 +0530

    feat: added dynamic content for career & articles pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,9 @@
 import { articleSchema } from "@schema/article";
+import { experienceSchema } from "@schema/experience";
+import { sideQuestSchema } from "@schema/side-quests";
 
 export const collections = {
   article: articleSchema,
+  experience: experienceSchema,
+  "side-quest": sideQuestSchema,
 };
