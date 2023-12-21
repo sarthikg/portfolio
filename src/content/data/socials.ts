cbea568c3dee337bf2a547260046b8299197c560
Sarthik Gupta <sarthikg@icloud.com>
2023-12-22 03:04:56 +0530

    feat: added footer

---
+++ src/content/data/socials.ts
@@ -0,0 +1,22 @@
+import type { Social } from "@schema/contact";
+
+export const socials: Social[] = [
+  {
+    name: "Twitter",
+    username: "sarthikg",
+    url: "https://www.twitter.com",
+    icon: "twitter",
+  },
+  {
+    name: "LinkedIn",
+    username: "sarthikg",
+    url: "https://www.linkedin.com/in",
+    icon: "linkedin",
+  },
+  {
+    name: "GitHub",
+    username: "sarthikg",
+    url: "https://www.github.com",
+    icon: "github",
+  },
+];
