6a72b181718100dda18964a3c4cd9b2ddaff1ef4
Sarthik Gupta <sarthikg@icloud.com>
2023-12-22 14:33:48 +0530

    feat: added sorting for experiences

--- src/facades/experience.ts
+++ src/facades/experience.ts
@@ -1,9 +1,26 @@
 import { getCollection, type CollectionEntry } from "astro:content";
 
+/**
+ * Retrieves experiences from the collection.
+ *
+ * @return {Promise<CollectionEntry<"experience">[]>} An array of experiences from the collection.
+ */
 export async function getExperiences(): Promise<
   CollectionEntry<"experience">[]
 > {
   const experiences = await getCollection("experience");
-  // TODO: Sort the experiences by date & isPrimary
+  experiences.sort((a, b) => {
+    const endDateA = (a.data.endDate || new Date()).getTime();
+    const endDateB = (b.data.endDate || new Date()).getTime();
+
+    if (endDateA > endDateB) {
+      return -1;
+    } else if (endDateA === endDateB) {
+      if (a.data.isPrimary && !b.data.isPrimary) {
+        return -1;
+      }
+    }
+  });
+
   return experiences;
 }
