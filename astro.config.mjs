401d3bea6e30aa8b9d0e7fd3566b68378645c518
Sarthik Gupta <sarthikg@icloud.com>
2023-12-28 02:10:02 +0530

    chore: replaced netlify with vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: netlify(),
   trailingSlash: "always",
+  adapter: vercel(),
 });
