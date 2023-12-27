572401ac033d9f3a784faa689a2966bcb8a66e1a
Sarthik Gupta <sarthikg@icloud.com>
2023-12-28 02:14:05 +0530

    chore: removed comment from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   trailingSlash: "always",
