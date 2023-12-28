47135f9449d065a696f66461e6e2d5d62fa2ef20
Sarthik Gupta <sarthikg@icloud.com>
2023-12-28 18:58:13 +0530

    refactor: replaced images with webp versions

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_ARTICLE_COVER =
-  "/src/assets/images/article/placeholder.jpg";
+  "/src/assets/images/article/placeholder.webp";
 export const DEFAULT_ARTICLE_ALT = "Placeholder Image for Article";
