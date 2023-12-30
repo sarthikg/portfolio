0652c047e17af34fea531e9cb290f824f1998ba4
Sarthik Gupta <sarthikg@icloud.com>
2023-12-31 03:56:18 +0530

    feat: added sitemap & robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
+  site: "https://sarthikg.com/",
   output: "hybrid",
   trailingSlash: "always",
   adapter: vercel(),
+  integrations: [sitemap()],
 });
