6fc80f7a12efad1e60d20f64cc9c8a88f6f4ba7a
Sarthik Gupta <sarthikg@icloud.com>
2024-01-09 16:09:50 +0530

    feat: enabled vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://sarthikg.com/",
   output: "hybrid",
   trailingSlash: "always",
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    imageService: true,
+  }),
   integrations: [sitemap()],
 });
