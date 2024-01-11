7c57a3f862ef652eb659527bd876f1e35480cc6b
Sarthik Gupta <sarthikg@icloud.com>
2024-01-11 20:55:27 +0530

    feat: migrated to development env file

--- deployment/dev/startup-script.sh
+++ deployment/dev/startup-script.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-echo Load Environment Variables
-echo "set -o allexport" >> ~/.bashrc
-echo "source ./deployment/dev/.env" >> ~/.bashrc
-echo "set +o allexport" >> ~/.bashrc
-source ~/.bashrc
-
 /bin/bash
