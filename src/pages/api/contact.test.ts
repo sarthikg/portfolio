e34cf91d78ea185bde0fe638e8a2fb347747b224
Sarthik Gupta <sarthikg@icloud.com>
2024-05-11 17:01:50 +0530

    test: added unit tests for contact api

--- src/pages/api/contact.test.ts
+++ src/pages/api/contact.test.ts
@@ -5,7 +5,7 @@ import nodemailer from "nodemailer";
 
 describe("contact api", () => {
   it("getTransporter returns the correct transporter object", () => {
-    vi.spyOn(nodemailer, "createTransport").mockImplementation((): any => {});
+    vi.spyOn(nodemailer, "createTransport");
     contact.getTransporter();
 
     expect(nodemailer.createTransport).toHaveBeenCalledWith({
@@ -28,31 +28,5 @@ describe("contact api", () => {
     expect(response).toContain("Name : John Doe");
     expect(response).toContain("Email : fDQ6g@example.com");
     expect(response).toContain("Message : Hello, World!");
-  });
-
-  it("sendEmail sends the correct email", () => {
-    const contactParams = ["fDQ6g@example.com", "John Doe", "Hello, World!"];
-    const emailBody = "Test Body";
-    const transporter = {
-      sendMail: vi.fn().mockImplementation((config: any): any => {}),
-    } as any;
-
-    vi.spyOn(contact, "getEmailBody").mockReturnValue(emailBody);
-    vi.spyOn(contact, "getTransporter").mockReturnValue(transporter);
-
-    contact.sendEmail(
-      ...(contactParams as [string, string, string]),
-      emailBody,
-    );
-
-    expect(contact.getTransporter).toHaveBeenCalled();
-    expect(contact.getEmailBody).toHaveBeenCalledWith(...contactParams);
-    expect(transporter.sendMail).toHaveBeenCalledWith({
-      from: import.meta.env.FROM_ADDRESS,
-      to: [import.meta.env.FROM_ADDRESS],
-      replyTo: [contactParams[0]],
-      subject: `[Contact Request] ${contactParams[2]}`,
-      html: emailBody,
-    });
   });
 });
