a40341dd2e53afaefdbaaa43d70b523a5ae109a9
Sarthik Gupta <sarthikg@icloud.com>
2024-08-14 21:57:03 +0530

    fix: image path in production

--- src/utils/image-buffer.ts
+++ src/utils/image-buffer.ts
@@ -4,13 +4,13 @@ import fetch from "node-fetch";
 
 export async function getImageBuffer(imagePath: string): Promise<Buffer> {
   if (process.env.NODE_ENV === "development") {
-    return readFileSync(
-      path.resolve(imagePath.replace(/\?.*/, "").replace("/@fs", "")),
-    );
+    const imageAbsolutePath = imagePath.replace(/\?.*/, "").replace("/@fs", "");
+    return readFileSync(path.resolve(imageAbsolutePath));
   }
-  const response = await fetch(
+  const imageAbsolutePath =
     imagePath.replace("/", `${import.meta.env.SITE}_vercel/image?url=`) +
-      "?width=1200&height=630",
-  );
+    "?width=1200&height=630";
+  console.log(imageAbsolutePath);
+  const response = await fetch(imageAbsolutePath);
   return await response.buffer();
 }
