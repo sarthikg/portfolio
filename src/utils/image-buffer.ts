fe1e0c313dac3c71db1dbd5945ef9f3f7e00f137
Sarthik Gupta <sarthikg@icloud.com>
2024-08-14 22:31:19 +0530

    fix: image path in production

--- src/utils/image-buffer.ts
+++ src/utils/image-buffer.ts
@@ -15,10 +15,12 @@ export async function getImageBuffer(imagePath: string): Promise<Buffer> {
         "/",
         `https://${import.meta.env.VERCEL_URL}/_vercel/og?url=`,
       ) + "?width=1200&height=630";
+  } else {
+    imageAbsolutePath =
+      imagePath.replace("/", `${import.meta.env.SITE}/_vercel/image?url=`) +
+      "?width=1200&height=630";
   }
-  imageAbsolutePath =
-    imagePath.replace("/", `${import.meta.env.SITE}/_vercel/image?url=`) +
-    "?width=1200&height=630";
+
   console.log(imageAbsolutePath);
   const response = await fetch(imageAbsolutePath);
   return response.buffer();
