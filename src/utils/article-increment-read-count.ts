10a3e38de99e0f0e8ef7d79da6dccae1fb31bad5
Sarthik Gupta <sarthikg@icloud.com>
2024-08-18 16:27:17 +0530

    feat: added conditional update of read-count

--- src/utils/article-increment-read-count.ts
+++ src/utils/article-increment-read-count.ts
@@ -1,6 +1,22 @@
+import { READ_ARTICLE_KEY } from "src/constants";
+
+/**
+ * Increment the read count of an article.
+ * @param slug - The slug of the article.
+ */
 export async function incrementArticleReadCount(
   slug: string,
 ): Promise<Response> {
+  // If article is already read by user, skip incrementation
+  const readArticles = localStorage.getItem(READ_ARTICLE_KEY);
+  let readArticleList: string[] = [];
+  if (readArticles) {
+    readArticleList = readArticles.split(", ");
+  }
+  if (readArticleList.includes(slug)) {
+    return;
+  }
+  // Increment the read count of article
   const response = await fetch("/api/article", {
     method: "POST",
     headers: {
--- src/utils/article-local-storage-cache.ts
+++ src/utils/article-local-storage-cache.ts
@@ -2,9 +2,9 @@ import { READ_ARTICLE_KEY } from "src/constants";
 
 /**
  * Store the current article as read article in local storage
- * @param articleSlug - slug of the article to be added
+ * @param slug - Slug of the article to be added
  */
-export function storeArticleAsRead(articleSlug: string): void {
+export function storeArticleAsRead(slug: string): void {
   const readArticles = localStorage.getItem(READ_ARTICLE_KEY);
 
   let readArticleList: string[] = [];
@@ -12,8 +12,8 @@ export function storeArticleAsRead(articleSlug: string): void {
     readArticleList = readArticles.split(", ");
   }
 
-  if (!readArticleList.includes(articleSlug)) {
-    readArticleList.push(articleSlug);
+  if (!readArticleList.includes(slug)) {
+    readArticleList.push(slug);
   }
 
   localStorage.setItem(READ_ARTICLE_KEY, readArticleList.join(", "));
