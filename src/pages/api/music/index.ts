dc538ed90917330e929817d66cb580fc6f632dc7
Sarthik Gupta <sarthikg@icloud.com>
2025-01-26 17:24:12 +0530

    chore: removed awaiting for fetch (#262)
    * feat: added cron job for spotify sync to apple-music
    
    * chore: disabled prerendering of api routes
    
    * chore: added log for response
    
    * chore: added log for music url

--- src/pages/api/music/index.ts
+++ src/pages/api/music/index.ts
@@ -6,23 +6,14 @@ import { spotifyPlaylists } from "./_constants";
 export const prerender = false;
 
 export async function GET(): Promise<Response> {
-  spotifyPlaylists.forEach(async (playlist) => {
-    const urlSearchParams = new URLSearchParams({
-      playlistId: playlist.id,
-    });
+  for (let i = 0; i < spotifyPlaylists.length; i++) {
+    const playlist = spotifyPlaylists[i];
+
+    const urlSearchParams = new URLSearchParams({ playlistId: playlist.id });
     console.log("Converting playlist:", playlist.name);
-    console.log(
-      `${import.meta.env.SITE}/api/music/sync?${urlSearchParams.toString()}`,
-    );
-    const response = await fetch(
-      `${import.meta.env.SITE}/api/music/sync?${urlSearchParams.toString()}`,
-    );
-    console.log(response);
-    if (response.status === 200) {
-      console.log("Converted playlist:", playlist.name);
-    } else {
-      console.log("Failed to convert playlist:", playlist.name);
-    }
-  });
+    const url = `${import.meta.env.SITE}/api/music/sync?${urlSearchParams.toString()}`;
+    const response = await fetch(url, { method: "GET" });
+    console.log("Response:", response.status);
+  }
   return new Response("Submitted!", { status: 200 });
 }
