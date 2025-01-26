e62330cb23ad47c53081a4e41c3c0ad0ca2e5f64
Sarthik Gupta <sarthikg@icloud.com>
2025-01-26 16:44:03 +0530

    feat: added cron job for spotify sync to apple-music

---
+++ src/pages/api/music/sync.ts
@@ -0,0 +1,26 @@
+import type { APIContext, Props } from "astro";
+
+export async function GET({ request }: APIContext<Props>): Promise<Response> {
+  try {
+    const playlistId = new URL(request.url).searchParams.get("playlistId");
+
+    if (!playlistId) {
+      return new Response("Missing playlistId", { status: 400 });
+    }
+
+    await fetch("https://playlistor.io/playlist", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Music-User-Token": import.meta.env.MUSIC_TOKEN,
+      },
+      body: JSON.stringify({
+        platform: "apple-music",
+        playlist: `https://open.spotify.com/playlist/${playlistId}`,
+      }),
+    });
+  } catch (error) {
+    return new Response("Something went wrong!", { status: 500 });
+  }
+  return new Response("Submitted!", { status: 200 });
+}
